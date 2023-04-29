493ca1efe1cce6a8648adcf085c15990f361b6e2
Ronald Kerama <keramaronald11@gmail.com>
2023-04-29 08:54:59 +0300

    Reverting back to original idea

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, { useState } from "react";
-import logo from './logo.svg';
+import React from "react";
+import LogIn from './login.js';
+
 import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
-
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-    </div>
+    <>
+      
+      <LogIn />
+    </>
   );
 }
 
