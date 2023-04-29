6fa6e755eab3fba29e130b8487d982bc97d558e2
Ronald Kerama <keramaronald11@gmail.com>
2023-04-29 08:37:17 +0300

    Restructing the log-in/Registration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
-import LogIn from './login.js';
-
+import React, { useState } from "react";
+import logo from './logo.svg';
 import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <>
-      
-      <LogIn />
-    </>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
   );
 }
 
--- src/logIn.js
+++ src/logIn.js
@@ -1,29 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-const inputs = document.querySelectorAll(".input");
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
 
-function addcl() {
-  let parent = this.parentNode.parentNode;
-  parent.classList.add("focus");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
 }
-
-function remcl() {
-  let parent = this.parentNode.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-function LogIn() {
-  inputs.forEach((input) => {
-    input.addEventListener("focus", addcl);
-    input.addEventListener("blur", remcl);
-  });
-
-  return <div>Login Form</div>;
-}
-
-export default LogIn;
-
-
-
