2299edef2559899ff271283fa5341558f0fbe90e
Ronald Kerama <keramaronald11@gmail.com>
2023-05-03 21:36:00 +0300

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,76 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
-function Footer(){
+function Footer() {
   return (
-    <div className="footer-container">
-    <div className="footer-section">
-      <h4>Company</h4>
-      <ul>
-        <li>About us</li>
-        <li>Newsroom</li>
-        <li>Investors</li>
-        <li>Careers</li>
-      </ul>
-    </div>
-    <div className="footer-section">
-      <h4>Products</h4>
-      <ul>
-        <li>Ride</li>
-        <li>Drive</li>
-        <li>Eats</li>
-        <li>Freight</li>
-      </ul>
-    </div>
-    <div className="footer-section">
-      <h4>Global Citizenship</h4>
-      <ul>
-        <li>Diversity and Inclusion</li>
-        <li>Accessibility</li>
-        <li>Safety</li>
-        <li>Sustainability</li>
-      </ul>
-    </div>
-    <div className="footer-section">
-      <h4>Travel</h4>
-      <ul>
-        <li>Airport rides</li>
-        <li>City-to-city</li>
-      </ul>
-    </div>
-    <div className="footer-section">
-      <h4>Language and Location</h4>
-      <ul>
-        <li>English (US)</li>
-        <li>Español</li>
-      </ul>
-    </div>
-  </div>
+    <footer className="footer-container">
+      <div className="footer-section">
+        <h4>About</h4>
+        <ul>
+          <li>Our story</li>
+          <li>Newsroom</li>
+          <li>Investors</li>
+          <li>Careers</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Products</h4>
+        <ul>
+          <li>Ride</li>
+          <li>Drive</li>
+          <li>Freight</li>
+          <li>Business</li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Cities</h4>
+        <ul>
+          <li>Nairobi</li>
+          <li>Mombasa</li>
+          <li>Kisumu</li>
+          <li>Eldoret</li>
+          <li>Nakuru</li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Support</h4>
+        <ul>
+          <li>Contact us</li>
+          <li>Help center</li>
+          <li>Accessibility</li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Legal</h4>
+        <ul>
+          <li>Terms of use</li>
+          <li>Privacy policy</li>
+          <li>Cookie policy</li>
+        </ul>
+      </div>
+      <div className="footer-section social">
+        <h4>Follow us</h4>
+        <ul>
+          <li>
+            <a href="#">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
-  }
+}
 
-
-export default Footer
+export default Footer;
