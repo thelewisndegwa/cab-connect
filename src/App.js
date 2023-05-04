76f04c4318882fb2fd5a339f26f84e564eea9e5e
thelewisndegwa <lewiskanyi77@gmail.com>
2023-05-04 08:45:14 +0300

    cabconnectai rendered

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
          
         </Routes>
       </BrowserRouter>
-    <Cabconnect/>
-      <Testmapform/>
+         <Testmapform/>
+         <Cabconnect/>
      <Footer/>
     </div>
   );
